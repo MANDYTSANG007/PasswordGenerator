56fffe032c60a3f34803bcc0bb2044d086a35479
Mandy Tsang <mandy.tsang007@gmail.com>
2021-12-24 11:18:35 -0800

    initial, improve functions to make all prompts working properly.

--- script.js
+++ script.js
@@ -20,61 +20,46 @@ function writePassword() {
 //if user choose the password length
 function howMany() {
   var lengthSelected = prompt("How many characters would you like your password to contain?"); //set lengthSelected=0 right at the beginning when user hasn't selected the length of the pw
-  if (lengthSelected >=8 || lengthSelected <=128 ){
-    //uppercase();
-    return lengthSelected;
-  }else if (lengthSelected < 8){
+  if (lengthSelected < 8){
     alert("Password length must be at least 8 characters.");
     return howMany();
   }else if (lengthSelected > 128){
     alert("Password length must be no more than 128 characters.")
     return howMany();
+  } else {
+    upperCase();
+    return lengthSelected;
   }
 };
-  
-  
-  
-  /*if (lengthSelected < 8) {
-    alert("Password length must be at least 8 characters.");
-    return howMany();
-  } else if (lengthSelected >= 128) {
-    alert("Password length must be no more than 128 characters.")
-    return howMany();
-  } else {
-    uppercase();
-    return lengthSelected; //store the selected length to the variable lengthSelected
-  }
-};*/
-
 //if user confirm whether or not to include uppercase 
-function uppercase() {
+function upperCase() {
   var uppercaseConfirmed = confirm("Click OK to confirm including lowercase characters.");
     if (uppercaseConfirmed == true) {  //get a Boolean result using confirm method
-      lowercase();
+      lowerCase();
       return uppercaseConfirmed;
-    } else {
-      return uppercase();
-  }
+    }
 };
 
 //if user confirm whether or not to include lowercase 
-function lowercase(selectLowercase) {
+function lowerCase(selectLowercase) {
   var lowercaseConfirmed = confirm("Click OK to confirm including lowercase characters.");
-  if (lowercase == true) {  //get a Boolean result using confirm method
+  if (lowercaseConfirmed == true) {  //get a Boolean result using confirm method
+    numericNum();
     return lowercaseConfirmed;
   }
 };
 //if user confirm whether or not to include numeric 
-function numeric(selectNumeric) {
+function numericNum(selectNumeric) {
   var numericConfirmed = confirm("Click OK to confirm including numeric numbers.");
-  if (numeric == true) {  //get a Boolean result using confirm method
+  if (numericNum == true) {  //get a Boolean result using confirm method
+    specialCharacter();
     return numericConfirmed;
   }
 };
 //if user confirm whether or not to include special characters 
-function special(selectSpecial) {
+function specialCharacter(selectSpecial) {
   var specialConfirmed = confirm("Click OK to confirm including special characters.");
-  if (special == true) {  //get a Boolean result using confirm method
+  if (specialConfirmed == true) {  //get a Boolean result using confirm method
     return specialConfirmed;
   }
 };
@@ -93,6 +78,7 @@ howMany();
   -if x <= 8, alert "must be at least 8"
   -if x >= 128, alert "must be no more than 128"
   -if x is within 8 and 128, store the selected result
+  -if x is <= 8 and x>=128, the function exit
 6. create prompt2for character types giving a choice of including lowercase, uppercase, numeric, and/or special characters
 7.check to see if at least one character type is selected
 8. if not , return to the missing question that hasn't been answered
