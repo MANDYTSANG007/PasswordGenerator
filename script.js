2cd378ab13017955dd670b35c7a0dd34d2112e10
Mandy Tsang <mandy.tsang007@gmail.com>
2021-12-24 10:32:23 -0800

    initial, improve functions

--- script.js
+++ script.js
@@ -13,64 +13,77 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password; //.value -> return the value property
-    
+
 }
+
+
 //if user choose the password length
-function howMany(selectLength){
-  var lengthSelected = 0; //set lengthSelected=0 right at the beginning when user hasn't selected the length of the pw
-  prompt("How many characters would you like your password to contain?");
-  for(i=8; i<=128; i++) { //set password length between the range of 8 and 128
-    if (selectLength <= 8){
-      alert("Password length must be at least 8 characters.");
-    }else if (selectLength >= 128){
-      alert("Password length must be no more than 128 characters.")
-    }else {
-      return lengthSelected; //store the selected length to the variable lengthSelected
-    }
+function howMany() {
+  var lengthSelected = prompt("How many characters would you like your password to contain?"); //set lengthSelected=0 right at the beginning when user hasn't selected the length of the pw
+  if (lengthSelected >=8 || lengthSelected <=128 ){
+    //uppercase();
+    return lengthSelected;
+  }else if (lengthSelected < 8){
+    alert("Password length must be at least 8 characters.");
+    return howMany();
+  }else if (lengthSelected > 128){
+    alert("Password length must be no more than 128 characters.")
+    return howMany();
   }
-console.log (lengthSelected);
+};
+  
+  
+  
+  /*if (lengthSelected < 8) {
+    alert("Password length must be at least 8 characters.");
+    return howMany();
+  } else if (lengthSelected >= 128) {
+    alert("Password length must be no more than 128 characters.")
+    return howMany();
+  } else {
+    uppercase();
+    return lengthSelected; //store the selected length to the variable lengthSelected
+  }
+};*/
+
 //if user confirm whether or not to include uppercase 
-function uppercase(selectUppercase){
-  var uppercaseConfirmed = "";
-  if (confirm("Click OK to confirm including uppercase characters.") == true){  //get a Boolean result using confirm method
-    return uppercaseConfirmed;
-  }else{
-    return
+function uppercase() {
+  var uppercaseConfirmed = confirm("Click OK to confirm including lowercase characters.");
+    if (uppercaseConfirmed == true) {  //get a Boolean result using confirm method
+      lowercase();
+      return uppercaseConfirmed;
+    } else {
+      return uppercase();
   }
-}
+};
+
 //if user confirm whether or not to include lowercase 
-function lowercase(selectLowercase){
-  var lowercaseConfirmed = "";
-  if (confirm("Click OK to confirm including lowercase characters.") == true){  //get a Boolean result using confirm method
+function lowercase(selectLowercase) {
+  var lowercaseConfirmed = confirm("Click OK to confirm including lowercase characters.");
+  if (lowercase == true) {  //get a Boolean result using confirm method
     return lowercaseConfirmed;
-  }else{
-    return
   }
-}
+};
 //if user confirm whether or not to include numeric 
-function numeric(selectNumeric){
-  var numericConfirmed = "";
-  if (confirm("Click OK to confirm including numeric numbers.") == true){  //get a Boolean result using confirm method
+function numeric(selectNumeric) {
+  var numericConfirmed = confirm("Click OK to confirm including numeric numbers.");
+  if (numeric == true) {  //get a Boolean result using confirm method
     return numericConfirmed;
-  }else{
-    return
   }
-}
+};
 //if user confirm whether or not to include special characters 
-function special(selectSpecial){
-  var specialConfirmed = "";
-  if (confirm("Click OK to confirm including special characters.") == true){  //get a Boolean result using confirm method
+function special(selectSpecial) {
+  var specialConfirmed = confirm("Click OK to confirm including special characters.");
+  if (special == true) {  //get a Boolean result using confirm method
     return specialConfirmed;
-  }else{
-    return
   }
-}
+};
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  //document.getElementById("password")
+howMany();
 /* pseudocode
 1. create record for passwordLength selection
 2. create record for lowercase selection
@@ -78,8 +91,8 @@ generateBtn.addEventListener("click", writePassword);
 4. create record for special character selection
 5. create prompt1  for the length of the pw that allows user to choose the length 8=<x=<128
   -if x <= 8, alert "must be at least 8"
-  -if x >= 128, alert "must be no more than 128" 
-  -if x is within 8 and 128, store the selected result 
+  -if x >= 128, alert "must be no more than 128"
+  -if x is within 8 and 128, store the selected result
 6. create prompt2for character types giving a choice of including lowercase, uppercase, numeric, and/or special characters
 7.check to see if at least one character type is selected
 8. if not , return to the missing question that hasn't been answered
