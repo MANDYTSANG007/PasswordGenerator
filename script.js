02ca8c373459a8d4a0b0f1e1dbf0c1eef6795c68
Mandy Tsang <mandy.tsang007@gmail.com>
2021-12-24 21:16:44 -0800

    initial, organize code structure

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ var generateBtn = document.querySelector("#generate");  //this will generate but
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numeric = "0123456789";
-var special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; // using backslash escape character to treat " specially 
-
+var special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; // use backslash escape character to treat " specially 
 
 
 // Write password to the #password input
@@ -13,62 +12,64 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password; //.value -> return the value property
-
 }
+//outcome of user's selections
+function afterPasswordButtonClick(){
+  var password_length = howMany(); //return integer
+  var confirm_uppercase = upperCase(); //return boolean
+  var confirm_lowercase = lowerCase(); //return boolean
+  var confirm_numeric = numericNum(); //return boolean
+  var confirm_special = special() //return boolean
+  var password = generatePassword(password_length, confirm_uppercase, confirm_lowercase, confirm_numeric, confirm_special) 
 
-
-//if user choose the password length
-function howMany() {
-  var lengthSelected = prompt("How many characters would you like your password to contain?"); //set lengthSelected=0 right at the beginning when user hasn't selected the length of the pw
-  if (lengthSelected < 8){
-    alert("Password length must be at least 8 characters.");
-    return howMany();
-  }else if (lengthSelected > 128){
-    alert("Password length must be no more than 128 characters.")
-    return howMany();
-  } else {
-    upperCase();
-    return lengthSelected;
-  }
-};
-//if user confirm whether or not to include uppercase 
-function upperCase() {
-  var uppercaseConfirmed = confirm("Click OK to confirm including lowercase characters.");
-    if (uppercaseConfirmed == true) {  //get a Boolean result using confirm method
-      lowerCase();
-      return uppercaseConfirmed;
+  //if user choose the password length
+  function howMany() {
+    var selectLength = prompt("How many characters would you like your password to contain?"); //set lengthSelected=0 right at the beginning when user hasn't selected the length of the pw
+    if (selectLength < 8){
+      alert("Password length must be at least 8 characters.");
+      return howMany();
+    }else if (selectLength > 128){
+      alert("Password length must be no more than 128 characters.")
+      return howMany();
+    }else {
+      return password_length;
     }
+  };
+  //if user confirm whether or not to include uppercase 
+  function upperCase() {
+    var confirmUppercase = confirm("Click OK to confirm including uppercase characters.");
+    if (confirmUppercase == true) {  //get a Boolean result using confirm method
+      return confirm_uppercase;
+    }
+  };
+  //if user confirm whether or not to include lowercase 
+  function lowerCase() {
+    var confirmLowercase = confirm("Click OK to confirm including lowercase characters.");
+    if (confirmLowercase == true) {  //get a Boolean result using confirm method
+      return confirm_lowercase;
+    }
+  };
+  //if user confirm whether or not to include numeric 
+  function numericNum() {
+    var confirmNumeric = confirm("Click OK to confirm including numeric numbers.");
+    if (confirmNumeric == true) {  //get a Boolean result using confirm method
+      return confirm_numeric;
+    }
+  };
+  //if user confirm whether or not to include special characters 
+  function special() {
+    var confirmSpecial = confirm("Click OK to confirm including special characters.");
+    if (confirmSpecial == true) {  //get a Boolean result using confirm method
+      return confirm_special;
+  }
 };
 
-//if user confirm whether or not to include lowercase 
-function lowerCase(selectLowercase) {
-  var lowercaseConfirmed = confirm("Click OK to confirm including lowercase characters.");
-  if (lowercaseConfirmed == true) {  //get a Boolean result using confirm method
-    numericNum();
-    return lowercaseConfirmed;
-  }
-};
-//if user confirm whether or not to include numeric 
-function numericNum(selectNumeric) {
-  var numericConfirmed = confirm("Click OK to confirm including numeric numbers.");
-  if (numericNum == true) {  //get a Boolean result using confirm method
-    specialCharacter();
-    return numericConfirmed;
-  }
-};
-//if user confirm whether or not to include special characters 
-function specialCharacter(selectSpecial) {
-  var specialConfirmed = confirm("Click OK to confirm including special characters.");
-  if (specialConfirmed == true) {  //get a Boolean result using confirm method
-    return specialConfirmed;
-  }
-};
 
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-howMany();
+
 /* pseudocode
 1. create record for passwordLength selection
 2. create record for lowercase selection
@@ -79,10 +80,28 @@ howMany();
   -if x >= 128, alert "must be no more than 128"
   -if x is within 8 and 128, store the selected result
   -if x is <= 8 and x>=128, the function exit
-6. create prompt2for character types giving a choice of including lowercase, uppercase, numeric, and/or special characters
-7.check to see if at least one character type is selected
-8. if not , return to the missing question that hasn't been answered
-9. if yes (meaning when all prompts are answered), the pw is generated
-10. alert that displays the password generated by the system
+6. create confirm1 for user to whether to include uppercase
+  -if user clicks ok, it returns true
+    -make a note on the result
+    -move on to the lowercase question
+  -if user clicks cancel, it returns false
+    -move on to the lowercase question
+7. create confirm2 for user to whether to include lowercase
+  -if user clicks ok, it returns true
+    -make a note on the result
+    -move on to the numeric question
+  -if user clicks cancel, it returns false
+    -move on to the numeric question
+8. create confirm3 for user to whether to include numeric numbers
+  -if user clicks ok, it returns true
+    -make a note on the result
+    -move on to the special characters question
+  -if user clicks cancel, it returns false
+9.check if at least one character type (at least one true is returned) is selected
+10. if not , prompt "at least one character type must be selected"
+11. if yes (meaning when all prompts are answered), the pw is generated
+  -first character of pw is generated
+  -second character of pw is generated
+  -repeat the step until it reaches the length of the password
+12. displays the password generated by the system
 */
-
